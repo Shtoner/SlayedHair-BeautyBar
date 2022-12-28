7c43c793ca6575376b386314c56fe11cb25ac6cb
Shtoner <zstoner57@gmail.com>
2022-12-28 17:05:48 -0600

    FirstCommit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
+import SLayed from './Slayed.png';
+import Cut1 from './Cut1.jpg'
+import Cut2 from './Cut2.jpg'
+import Cut3 from './Cut3.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+        <img src={SLayed} className="Top" alt="logo" />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="text">
+        Services include braids, barbering, starter locs & retwists, coils, twists, silk press, rollersets, relaxers, perms, color, hilites, weaves and more.
+        </div>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.instagram.com/slayedhairandbeautybar/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check out our Instagram!
         </a>
+        <br></br>
+        <img src={Cut1} className='exImage'></img>
+        <img src={Cut2} className='exImage'></img>
+        <img src={Cut3} className='exImage'></img>
+
       </header>
     </div>
   );
