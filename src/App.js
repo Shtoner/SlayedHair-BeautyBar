04935b20cd3f2da38c7f4c7c08936fa6127b317b
Shtoner <zstoner57@gmail.com>
2022-12-29 09:34:28 -0600

    cssAnimations

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import SLayed from './Slayed.png';
 import Cut1 from './Cut1.jpg'
 import Cut2 from './Cut2.jpg'
 import Cut3 from './Cut3.jpg'
+
 import './App.css';
 
 function App() {
@@ -10,20 +11,10 @@ function App() {
         <img src={SLayed} className="Top" alt="logo" />
       <header className="App-header">
         <div className="text">
-        Services include braids, barbering, starter locs & retwists, coils, twists, silk press, rollersets, relaxers, perms, color, hilites, weaves and more.
-        </div>
-        <br></br>
-        <div className="text">
-        Call Us!: (304) 267-2700
-        </div>
-        <br></br>
-        <br></br>
-        <div className="text">
-        2 Locations: 416 W King ST Martinsburg 25401 & 306 W Washington ST Charles Town 25414 call 4102584914
-        </div>
-        <br></br>
-        <div className="text">
-        Hours: 9AM-6PM
+        <p>Services include braids, barbering, starter locs & retwists, coils, twists, silk press, rollersets, relaxers, perms, color, hilites, weaves and more.</p>
+        <p><div className='slidel' style={{color:'rgb(243, 215, 57)'}}>Call Us!:</div> (304) 267-2700</p>
+        <p><div className='slider'style={{color:'rgb(243, 215, 57)'}}>2 Locations:</div><div> 416 W King ST Martinsburg 25401 & 306 W Washington ST Charles Town 25414 call 4102584914</div></p>
+        <p>Hours: 9AM-6PM</p>
         </div>
         <a
           className="App-link"
